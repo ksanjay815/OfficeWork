b76d3bc03f3d53a4c89c2fa82a4ceac503f6e870
ALGRACE123 <salmanmarolix@gmail.com>
2023-12-05 05:22:45 +0530

    updated

--- src/main/java/Practice/Interface_Class.java
+++ src/main/java/Practice/Interface_Class.java
@@ -1,28 +1,29 @@
 package Practice;
 
+// Interface
+interface Animals {
+	public void animalSound(); // interface method (does not have a body)
 
-	// Interface
-	interface Animals {
-	  public void animalSound(); // interface method (does not have a body)
-	  public void sleep(); // interface method (does not have a body)
+	public void sleep(); // interface method (does not have a body)
+}
+
+// Pig "implements" the Animal interface
+class Pigs implements Animals {
+	public void animalSound() {
+		// The body of animalSound() is provided here
+		System.out.println("The pig says: wee wee");
 	}
 
-	// Pig "implements" the Animal interface
-	class Pigs implements Animals {
-	  public void animalSound() {
-	    // The body of animalSound() is provided here
-	    System.out.println("The pig says: wee wee");
-	  }
-	  public void sleep() {
-	    // The body of sleep() is provided here
-	    System.out.println("Zzzzzz");
-	  }
+	public void sleep() {
+		// The body of sleep() is provided here
+		System.out.println("Zzzzzz");
 	}
+}
 
-	public class Interface_Class {
-	  public static void main(String[] args) {
-	    Pigs myPig = new Pigs();  // Create a Pig object
-	    myPig.animalSound();
-	    myPig.sleep();
-	  }
+public class Interface_Class {
+	public static void main(String[] args) {
+		Pigs myPig = new Pigs(); // Create a Pig object
+		myPig.animalSound();
+		myPig.sleep();
 	}
+}
